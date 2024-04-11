85e9ad9f607aa0bd00a51901170de53ef12e15c6
John Mota <johnmarcos.mota@gmail.com>
2024-04-11 14:03:07 -0300

    refactor

--- install_apps.sh
+++ install_apps.sh
@@ -1,61 +1,63 @@
 #!/bin/bash
 
-# Atualizar a lista de pacotes
+# Update package list
 sudo apt update
 
 sudo apt upgrade -y
 
-# install curl
+# Install curl
 sudo apt install curl -y
 
-# install git
+# Install git
 sudo apt install git -y
 
-# install font-hack
+# Install Hack font
 sudo apt-get install fonts-hack-ttf -y
 
-# Instalação do MySQL
+# MySQL Installation
 sudo apt install mysql-client-core-8.0
 
-# Instalação do MySQL Workbench
+# MySQL Workbench Installation
 wget https://dev.mysql.com/get/Downloads/MySQLGUITools/mysql-workbench-community_8.0.28-1ubuntu20.04_amd64.deb
 sudo dpkg -i mysql-workbench-community_8.0.28-1ubuntu20.04_amd64.deb
 
-# instal MVN
- curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+# install NVM
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 
-
-# Download do arquivo .deb do Google Chrome
+# Download Google Chrome .deb file
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 
-# Instalação do Google Chrome a partir do arquivo .deb baixado
+# Install Google Chrome from downloaded .deb file
 sudo dpkg -i google-chrome-stable_current_amd64.deb
 
-# Se houver dependências não satisfeitas, você pode corrigi-las com o seguinte comando
+# If there are unmet dependencies, you can fix them with the following command
 sudo apt-get install -f -y
 
-# Download do Discord
+# Download Discord
 wget -O discord.deb "https://discord.com/api/download?platform=linux&format=deb"
 
-# Instalação do Discord
+# Install Discord
 sudo dpkg -i discord.deb
 sudo apt-get install -f -y
 
-# Remover os arquivos .deb após a instalação (opcional)
+# Install VSCode
+wget "https://go.microsoft.com/fwlink/?LinkID=760868" -O vscode.deb
+sudo dpkg -i vscode.deb
+sudo apt install -f
+# Remove .deb files after installation (optional)
 rm discord.deb gimp.deb telegram.deb hero_games.deb google-chrome-stable_current_amd64.deb mysql-apt-config_0.8.16-1_all.deb mysql-workbench-community_8.0.28-1ubuntu20.04_amd64.deb
 
-# Flat-Remix
+# Flat-Remix Theme
 sudo add-apt-repository ppa:daniruiz/flat-remix -y && sudo apt-get update && sudo apt-get install flat-remix-gtk -y && sudo apt-get install flat-remix -y
-#install flatpak
+# Install flatpak
 sudo apt install flatpak -y
 sudo apt install gnome-software-plugin-flatpak -y
 flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 
-# install themes remix
+# Install Remix themes
 sudo add-apt-repository ppa:daniruiz/flat-remix -y && sudo apt-get update && sudo apt-get install flat-remix-gtk -y && sudo apt-get install flat-remix -y
 
-
-# Instalar aplicativos flatpak
+# Install flatpak applications
 
 # InteliJ
 flatpak install flathub com.jetbrains.IntelliJ-IDEA-Community -y
@@ -63,10 +65,8 @@ flatpak install flathub com.jetbrains.IntelliJ-IDEA-Community -y
 # WhatsApp
 flatpak install flathub io.github.mimbrero.WhatsAppDesktop -y
 
-# Emulador Nitendo
+# Nintendo Emulator
 flatpak install flathub org.ryujinx.Ryujinx -y
 
-
-# Exibe uma mensagem de conclusão
-echo "A instalação foi concluída."
-
+# Display completion message
+echo "Installation completed."
