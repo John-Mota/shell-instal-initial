9a994c3418ccecacfd40226b31c30005ae97bbe9
John Mota <john.oliveira@saude.ce.gov.br>
2024-12-26 10:49:07 -0300

    add mise

--- instal.sh
+++ instal.sh
@@ -38,8 +38,12 @@ sudo apt-get update
 sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
 
-# Java Development Kit
-sudo apt-get -y install openjdk-11-jdk -y
+if ! getent group docker > /dev/null; then
+    sudo groupadd docker
+fi
+sudo usermod -aG docker $USER
+sudo systemctl restart docker
+
  
 
 # PostgresQL
@@ -91,6 +95,15 @@ sudo dpkg -i vscode.deb
 sudo apt install -f -y
 # Remove .deb files after installation (optional)
 rm discord.deb gimp.deb telegram.deb hero_games.deb google-chrome-stable_current_amd64.deb mysql-apt-config_0.8.16-1_all.deb mysql-workbench-community_8.0.28-1ubuntu20.04_amd64.deb
+
+
+# Mise 
+sudo apt update -y && sudo apt install -y gpg sudo wget curl
+sudo install -dm 755 /etc/apt/keyrings
+wget -qO - https://mise.jdx.dev/gpg-key.pub | gpg --dearmor | sudo tee /etc/apt/keyrings/mise-archive-keyring.gpg 1> /dev/null
+echo "deb [signed-by=/etc/apt/keyrings/mise-archive-keyring.gpg arch=amd64] https://mise.jdx.dev/deb stable main" | sudo tee /etc/apt/sources.list.d/mise.list
+sudo apt update
+sudo apt install -y mise
 
 # Install flatpak
 sudo apt install flatpak -y
