dacb36e107e68b0a577aa31c691b12fa2dbd19ad
John Mota <johnmarcos.mota@gmail.com>
2025-12-10 08:45:16 -0300

    refactor: Ajusta inicialização de ferramentas e configuração do fish

--- assets/config.fish
+++ assets/config.fish
@@ -2,13 +2,44 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
 end
 set -U fish_greeting ""
-fzf --fish | source
-zoxide init fish | source
-set SPACEFISH_PROMPT_ADD_NEWLINE false
-eval "$(~/.local/bin/mise activate fish | source)"
-export NODE_OPTIONS="--max-old-space-size=4096"
-starship init fish | source
 
+# PATH completo - PRECISA VIR ANTES DE TUDO
+#set -gx PATH /usr/local/bin /usr/bin /bin /usr/local/sbin /usr/sbin /sbin $HOME/bin $HOME/.local/bin $HOME/flutter/bin
+
+set -gx PATH /usr/local/bin /usr/bin /bin /usr/local/sbin /usr/sbin /sbin $HOME/bin $HOME/.local/bin $HOME/flutter/bin $HOME/.pub-cache/bin /usr/local/flutter/bin/cache/dart-sdk/bin
+
+# FZF (comente se não tiver instalado)
+if type -q fzf
+    fzf --fish | source
+end
+
+# Zoxide (comente se não tiver instalado)
+if type -q zoxide
+    zoxide init fish | source
+end
+
+# Starship prompt
+if type -q starship
+    starship init fish | source
+end
+
+# Mise
+if test -f $HOME/.local/bin/mise
+    eval "$(~/.local/bin/mise activate fish | source)"
+end
+
+# Node options
+set -gx NODE_OPTIONS "--max-old-space-size=4096"
+
+# Spacefish config
+set SPACEFISH_PROMPT_ADD_NEWLINE false
+
+# FZF config
+set -gx FZF_CTRL_T_OPTS "
+  --style full
+  --walker-skip .git,node_modules,target
+  --preview 'bat -n --color=always {}'
+  --bind 'ctrl-/:change-preview-window(down|hidden|)'"
 
 # Alias do Sistema
 alias configFish='sudo nano ~/.config/fish/config.fish'
@@ -17,6 +48,7 @@ alias ls='eza --color=always --git --no-filesize --icons=always --group-director
 alias cat='batcat --style=plain --paging=never'
 alias cd='z'
 alias l='ls -la'
+alias conect-mv='ssh -i ~/.ssh/id_ed25519 ubuntu@137.131.195.115'
 
 # Alias Git
 alias gcd='git checkout develop'
@@ -26,10 +58,3 @@ alias gcb='git checkout -b'
 alias checkout='git checkout'
 alias clone='git clone'
 
-
-
-export FZF_CTRL_T_OPTS="
-  --style full
-  --walker-skip .git,node_modules,target
-  --preview 'bat -n --color=always {}'
-  --bind 'ctrl-/:change-preview-window(down|hidden|)'"
